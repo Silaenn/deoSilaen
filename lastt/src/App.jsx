12ca249d6b03ac149a7245087cccd291802e5e42
Deo Silaen <131638765+Silaenn@users.noreply.github.com>
2023-10-03 14:42:46 +0700

    lastt

---
+++ lastt/src/App.jsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import KelasPage from "./pages/KelasPage";
+import SyaratKatenPage from "./pages/SyaratKatenPage";
+import TestimonialPage from "./pages/TestimonialPage";
+import FaqComponent from "./components/FaqComponent";
+import NavbarComponent from "./components/NavbarComponent";
+import FooterComponent from "./components/FooterComponent";
+
+function App() {
+  return (
+    <div>
+      <NavbarComponent />
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/kelas" Component={KelasPage} />
+        <Route path="/testimonial" Component={TestimonialPage} />
+        <Route path="/faq" Component={FaqComponent} />
+        <Route path="/syaratketen" Component={SyaratKatenPage} />
+      </Routes>
+      <FooterComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ lastt/src/pages/SyaratKatenPage.jsx
@@ -0,0 +1,218 @@
+import { Container, Row, Col } from "react-bootstrap";
+import FaqComponent from "../components/FaqComponent";
+
+const SyaratKatenPage = () => {
+  return (
+    <div className="syarat-ketentuan-page">
+      <div className="syarat-ketentuan min-vh-100">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="fw-bold text-center mb-2">Syarat & Ketentuan</h1>
+              <p className="text-center">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </Col>
+          </Row>
+          <Row className="pt-5">
+            <Col>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit
+                hic consequatur reprehenderit adipisci odio error quae, quasi ea
+                omnis consequuntur eum doloribus, ducimus officiis beatae optio,
+                ratione at iste quis. Perferendis, quos quisquam. Ex, enim.
+                Dolorem quos tempora quidem ipsum, veniam ad excepturi accusamus
+                laborum reiciendis tempore consectetur voluptatibus sit.
+              </p>
+            </Col>
+          </Row>
+          <Row className="py-3">
+            <Col>
+              <h4 className="fw-bold">1. Lorem</h4>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam, reprehenderit quidem nostrum ratione dolor soluta
+                officiis libero assumenda accusantium fugit odit quae saepe cum
+                maxime, alias dolorum voluptate, iure molestiae. Ex quam quia
+                repellendus natus dignissimos esse, ipsa in beatae.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+                eveniet, reprehenderit est quo asperiores totam commodi facere
+                unde inventore, temporibus libero aliquid voluptas magnam
+                laudantium minus doloribus officia tempore modi, nihil
+                blanditiis ab neque. Sunt reprehenderit dolor molestiae magni
+                fuga in modi quis sit aspernatur earum perspiciatis accusamus,
+                impedit ea voluptatum voluptatibus! Earum minus ut optio,
+                tempore nisi consequatur deleniti!
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Perferendis sunt repellendus harum dolorem, porro doloribus
+                illum quibusdam, pariatur, voluptate laborum officia aliquam
+                impedit eligendi dolorum sed! Unde facere perferendis dolores
+                velit nisi ut est consectetur ipsa sapiente ipsam expedita
+                dolorum, numquam ex iusto voluptatem, cum a. Error placeat omnis
+                itaque commodi rerum consectetur sunt tenetur. Hic dolorem a
+                nihil vitae architecto, nostrum quaerat asperiores cupiditate?
+                Facilis veniam, et laudantium, perspiciatis doloremque facere
+                hic eligendi cumque officia aliquid ipsum fuga quidem?
+              </p>
+            </Col>
+          </Row>
+          <Row className="py-3">
+            <Col>
+              <h4 className="fw-bold">2. Lorem</h4>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Exercitationem voluptates fuga sunt provident est beatae eos
+                aperiam velit quia. Illum quo dolorem quae porro corrupti sunt
+                quasi doloremque odio, velit ipsam libero facilis quaerat quos
+                beatae architecto provident saepe ab.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque a
+                sunt atque ipsum incidunt iure aspernatur! Facere vitae tempore
+                ipsum maiores! Quis rerum officiis, excepturi odit laboriosam
+                eos consequuntur id perspiciatis praesentium dicta,
+                necessitatibus, natus harum suscipit corrupti ducimus cupiditate
+                et. Velit, sed et deserunt nulla esse aperiam. Blanditiis
+                consequatur repudiandae deserunt omnis expedita doloremque
+                aliquam esse quis corrupti dolorem id repellendus iure
+                voluptatibus, ipsum eligendi quia reiciendis sed repellat
+                aspernatur aliquid nemo libero? Delectus voluptates fugit nemo
+                quidem itaque!
+              </p>
+            </Col>
+          </Row>
+          <Row className="py-3">
+            <Col>
+              <h4 className="fw-bold ">3. Lorem</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore
+                eos magnam impedit. Ipsa perspiciatis voluptates accusantium
+                animi fuga maxime qui expedita ipsum, libero sapiente laudantium
+                beatae ipsam eos inventore vitae! Illum, sapiente alias? Odio,
+                quisquam harum? Repellendus culpa vero minus.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Reprehenderit laborum mollitia iure totam praesentium. Sapiente
+                non animi deserunt doloribus fugiat nisi adipisci ex nesciunt
+                saepe. Voluptate soluta mollitia laudantium, corporis cum
+                expedita sint omnis pariatur placeat quisquam eum, adipisci
+                tempora consequatur suscipit earum libero est, molestiae quas
+                unde vel explicabo natus! Quam, dignissimos quas. Omnis iste
+                facere repudiandae impedit doloremque, officia labore sint,
+                debitis aut ut, minus sed non modi vero cum rem velit iusto
+                saepe corrupti ullam autem excepturi accusamus laudantium. Non
+                odio quo saepe? Officia minus iure eius necessitatibus
+                consequatur, dicta nemo a, repudiandae praesentium at
+                perspiciatis fugiat? Facilis nostrum, quae hic quia facere iure
+                nesciunt excepturi provident distinctio sapiente sint
+                necessitatibus quam voluptate, impedit atque placeat consequatur
+                nemo dignissimos quod molestias soluta sed assumenda ratione.
+                Vitae expedita temporibus dicta molestiae ipsum aperiam
+                voluptas, excepturi laborum vel quod explicabo provident
+                accusamus illo. Dolores iste debitis beatae voluptatem totam.
+                Commodi dolor fugit deserunt quae suscipit odit ut atque est
+                voluptas, nam alias blanditiis quia totam ducimus placeat ad
+                minus provident eligendi unde autem aliquid iure sequi quasi
+                adipisci! Atque, veritatis? Provident voluptates ipsum officiis
+                illo ex. Nulla, molestias autem velit maxime necessitatibus quam
+                itaque labore maiores cum? Sint laborum, quasi, autem quod
+                commodi fuga dolorem rerum est ipsa inventore ut ipsam soluta
+                nesciunt rem, sit officia repudiandae? A totam maxime deleniti
+                nobis distinctio, officia hic voluptates, impedit consequuntur
+                aliquam consequatur, maiores asperiores magnam laudantium
+                quisquam. Quia consectetur quos et, iusto ratione harum fuga
+                neque quibusdam, cupiditate corrupti a animi labore praesentium
+                voluptatibus quidem. Impedit qui itaque explicabo dolore,
+                adipisci, perspiciatis quas expedita deserunt voluptatibus
+                accusamus, ex iure animi tempore aperiam vero ducimus? A tempora
+                nulla accusantium voluptatum velit architecto laudantium, eum
+                optio, in rem necessitatibus eaque deserunt? Est, enim dolore
+                quis dignissimos quia non blanditiis sequi doloribus repudiandae
+                rerum necessitatibus, ex rem vel. Exercitationem rerum obcaecati
+                dicta maxime saepe! Consequuntur culpa dicta ea, eius
+                necessitatibus illo quas nisi fugit optio praesentium?
+                Voluptates recusandae facere laborum, quod repellat enim
+                similique, sequi quam quibusdam obcaecati qui pariatur quae ab
+                optio explicabo quasi modi tempore ea possimus, vero error
+                fugit. Rem quibusdam, adipisci, sed consequatur nostrum nesciunt
+                cupiditate voluptates explicabo nulla a quod unde magni omnis
+                officia dicta nihil officiis optio vero ad consectetur illo
+                tempora architecto consequuntur. Iure explicabo, perferendis,
+                vitae voluptas quis reprehenderit veniam officia placeat numquam
+                officiis eos sunt expedita deserunt? Autem tempora voluptates
+                maiores ex repudiandae iusto iure quidem adipisci earum possimus
+                beatae, consequatur illo, voluptatem nihil laudantium soluta
+                provident debitis quae, deserunt tenetur eligendi cupiditate
+                asperiores perferendis! In at error laboriosam aliquid
+                voluptates a iure nihil voluptas magnam saepe ducimus quia odio
+                amet consectetur explicabo ipsum corrupti, voluptatem eaque
+                facere maiores? Repellat, dignissimos possimus, laboriosam quod
+                sed accusantium nobis voluptatum iste maxime ipsum obcaecati!
+                Quibusdam perferendis odit ea, deleniti doloribus eaque, ipsa
+                aliquid, voluptatem deserunt soluta nam praesentium delectus?
+                Iste ducimus inventore dolorum facilis laudantium ad odit
+                necessitatibus hic amet, possimus laboriosam explicabo. Culpa
+                earum laboriosam voluptates corrupti laborum provident
+                dignissimos. Ullam fugit similique ipsa optio veritatis
+                suscipit, ipsam consequuntur facere non facilis placeat fugiat
+                dicta expedita culpa nulla, fuga magnam, aut adipisci aspernatur
+                libero minus totam rem. Cumque, adipisci! Dolorum repellendus,
+                corrupti sint quas culpa delectus vitae dignissimos natus
+                similique at eaque maiores veritatis ipsam reprehenderit dolores
+                voluptatum architecto et cupiditate deserunt inventore amet. Cum
+                voluptatibus earum quos repudiandae! Similique excepturi ex
+                velit porro. A illum molestiae odio aut tempora qui officiis
+                nihil eveniet amet esse blanditiis tenetur harum, sunt beatae
+                alias sint minima mollitia culpa, odit necessitatibus quo! Iure
+                voluptatum animi minus esse sit. Nihil laudantium, qui, vitae
+                rem deserunt atque esse id odio neque accusantium non blanditiis
+                harum assumenda repudiandae! Repudiandae nostrum, consequuntur
+                rem debitis saepe tenetur corporis beatae quae minus voluptatum
+                iste laboriosam sint! Voluptates aspernatur ut assumenda debitis
+                veniam totam deserunt dolorem quae dolore eum illo,
+                exercitationem reiciendis cum deleniti quia quibusdam quas
+                sapiente distinctio modi perferendis sit? Eos perspiciatis unde
+                delectus laudantium eligendi aspernatur earum cumque quae
+                suscipit voluptatem maiores culpa est voluptate neque, qui
+                dolores aliquam non quisquam! Minima blanditiis cum alias quod
+                tempore amet veritatis quam praesentium recusandae sapiente
+                totam rem ad fugit exercitationem iusto dolore vero tenetur
+                cumque nam, nesciunt id dolor unde doloribus. Libero consectetur
+                id dignissimos odio similique. In, commodi nam fuga sequi
+                beatae, dolore quasi asperiores impedit architecto adipisci sed
+                aut quisquam porro iusto rerum! Itaque alias molestiae ratione
+                tenetur laboriosam reiciendis voluptate maxime autem assumenda
+                ipsa sapiente eaque, animi nostrum quam excepturi a quas, libero
+                possimus quia laborum voluptatem asperiores! Nulla fuga repellat
+                quam, ab expedita fugiat consequatur molestias dolorem
+                reiciendis quis sequi quia delectus nam aut facere, nostrum
+                numquam. Nisi obcaecati cumque porro ad consectetur quo non
+                pariatur corrupti accusantium minima inventore veritatis beatae
+                suscipit, temporibus odit, recusandae ipsum! Id autem, quisquam
+                ex quidem velit officia expedita sunt quos nobis quis libero
+                vel?
+              </p>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam
+                saepe quia dicta, repudiandae ad enim rem expedita voluptatibus
+                aut omnis molestiae veritatis id illum nostrum facere commodi
+                consectetur error ullam quibusdam praesentium, quos culpa
+                laboriosam deserunt maxime! Modi ipsum dolorum eius id iusto
+                eveniet recusandae, quis libero a sint sequi eligendi quae,
+                nesciunt iste quibusdam explicabo facere doloribus fuga maiores?
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <FaqComponent />
+    </div>
+  );
+};
+
+export default SyaratKatenPage;
